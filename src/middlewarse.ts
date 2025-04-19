346ec9338ff01d25ad647d05e534d945d239e7ad
thinesjs <thinesfb@gmail.com>
2025-04-19 12:00:23 +0800

    chore: removed prisma db related files and install commit & tag

--- src/middlewarse.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-// const isHomeRoute = createRouteMatcher(["/"]);
-
-export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
