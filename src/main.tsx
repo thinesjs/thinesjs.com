c7e88c71990036b174be3f93247e54c521f21822
thinesjs <thinesjs@gmail.com>
2025-12-25 21:25:15 +0800

    feat(ci/ui): release workflow, variable sans font

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 
+import geistWoff2 from "@fontsource-variable/geist/files/geist-latin-wght-normal.woff2?url";
+
+const preloadLink = document.createElement("link");
+preloadLink.rel = "preload";
+preloadLink.as = "font";
+preloadLink.type = "font/woff2";
+preloadLink.href = geistWoff2;
+preloadLink.crossOrigin = "anonymous";
+document.head.appendChild(preloadLink);
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <ThemeProvider defaultTheme="dark" storageKey="app-ui-theme">
